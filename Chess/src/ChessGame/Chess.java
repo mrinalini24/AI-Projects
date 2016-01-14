34efd6faeda2211fe85b0254ead33516e7ee2d81
mrinalini24 <mrinalini24@hotmail.com>
2016-01-14 12:51:57 -0500

    Initial Commit

---
+++ Chess/src/ChessGame/Chess.java
@@ -0,0 +1,11 @@
+package ChessGame;
+import MainFrame.ChessFrame.MainFrame;
+
+
+public class Chess {
+	public static void main(String[] args) {
+		MainFrame main =new MainFrame() ;
+	}
+
+	public Object main;
+}
