114a6df86a592dcbd725399324061e14effca2cf
mrinalini24 <mrinalini24@hotmail.com>
2017-01-16 14:06:43 -0800

    delete pathfinder

--- Pathfinder/com/nwgjb/pathfinder/search/Search.java
+++
@@ -1,9 +0,0 @@
-package com.nwgjb.pathfinder.search;
-import com.nwgjb.pathfinder.model.Model;
-import com.nwgjb.pathfinder.model.Node;
-import com.nwgjb.pathfinder.model.Path;
-
-
-public interface Search {
-	Path search(Model model, Node start, Node dest);
-}	
--- Pathfinder/com/nwgjb/pathfinder/ui/Painter.java
+++
@@ -1,7 +0,0 @@
-package com.nwgjb.pathfinder.ui;
-
-import java.awt.Graphics2D;
-
-public interface Painter {
-	void paint(Graphics2D g);
-}
